9ce75cedb89dc883a1d252dc484813892bd38d64
neiser.montano <desarrollo3@cosmitet.net>
2021-03-23 11:58:01 -0500

    constructores

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace consolaCurso
 {
@@ -8,7 +9,7 @@ namespace consolaCurso
         {
         }
 
-        static void Main(String[] arg)
+      /*  static void Main(String[] arg)
         {
             //Persona persona = new Persona();
             //persona.nombre = "neiser";
@@ -18,14 +19,19 @@ namespace consolaCurso
             //Console.WriteLine(persona.nombre);
             //Console.WriteLine(" salario anual : "+persona.salarioAnual);
 
+            // TIPOS Y REFERENCIAS 
+            //Persona persona = new Persona();
+            //var numero = 5;
+            //Aumentar(ref numero);
+            //Console.WriteLine(numero); 
 
-            Persona persona = new Persona();
-            var numero = 5;
-            Aumentar(ref numero);
-            Console.WriteLine(numero);
+            // === CONSTRUCTORES 
 
 
-        }
+
+
+
+        } */
         public static void Aumentar(ref int aumentar)
         {
             aumentar++;
@@ -36,6 +42,25 @@ namespace consolaCurso
 
     class Persona
     {
+        public Persona() {
+            telefonos = new List<string>();
+        }
+
+        public Persona(String nombre , int salariomensual)
+            :this()
+        {
+            nombre = nombre;
+            salariomensual = salariomensual;
+        }
+
+        static void Main(String[] arg)
+        {
+            Persona persona = new Persona();
+            persona.telefonos.Add("454121212");
+            Persona persona2 = new Persona("NEISER" , 15000);
+            persona2.telefonos.Add("77757575");
+            Console.WriteLine(persona.telefonos);
+        }
 
 
         public String nombre { get; set; }
@@ -45,7 +70,7 @@ namespace consolaCurso
                 return salariomensual * 12;   }
         }
 
-
+        public List<String> telefonos { get; set; }
 
 
     }
