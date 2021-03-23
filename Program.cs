8f1fce6f2d56f3575935e1a85be6bbbf6c11bc2c
neiser.montano <desarrollo3@cosmitet.net>
2021-03-23 10:51:04 -0500

    campos y propiedades

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace consolaCurso
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        static void Main(String[] arg)
+        {
+            Persona persona = new Persona();
+            persona.nombre = "neiser";
+            persona.nombre = "paola";
+            Console.WriteLine(persona.nombre);
+
+        }
+
+    }
+
+
+    class Persona
+    {
+
+
+        public String nombre { get; set; }
+
+
+
+
+    }
+}
